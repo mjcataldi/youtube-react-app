d9ac7f66e2940afb9c69c21269fbf99e8280cb92
Matt Cataldi <mcataldi@apics.org>
2018-12-10 16:19:54 -0600

    Refined VideoItem in VideoList component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,40 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import YouTube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      videos: []
+    };
   }
 
   onTermSubmit = async term => {
-    const videos = await YouTube.get(`/search?q=${term}`);
-    console.log(videos.data.items);
+    const response = await YouTube.get(`/search?q=${term}`);
+    this.setState({ videos: response.data.items });
+    // console.log(this.state.videos);
   };
+
+  countVideos() {
+    const videoCount = this.state.videos.length;
+    console.log(this.state.videos.items);
+
+    return videoCount === 0
+      ? "Enter a search term"
+      : `I have ${videoCount} videos.`;
+  }
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar
+          onFormSubmit={this.onTermSubmit}
+          Videos={this.state.videos.items}
+        />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ src/components/VideoItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./VideoItem.css";
+
+const VideoItem = ({ video }) => {
+  return (
+    <div className="video-item item">
+      <img
+        className="ui image;"
+        src={video.snippet.thumbnails.default.url}
+        alt={video.snippet.title}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+        <div className="description">{video.snippet.description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoItem;
